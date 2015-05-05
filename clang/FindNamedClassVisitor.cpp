c1c83b282d895208d75c6dd79f7e3b8e69c1e512
Gabe Kaptchuk <gkaptchuk@jhu.edu>
2015-05-05 16:34:59 -0400

    Update on the clang tool.  Name still wrong

---
+++ clang/FindNamedClassVisitor.cpp
@@ -0,0 +1,61 @@
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Tooling/Tooling.h"
+//#include "clang/Tooling/CommonOptionsParser.h"
+//#include "llvm/Support/CommandLine.h"
+
+using namespace clang;
+
+class FindNamedClassVisitor
+  : public RecursiveASTVisitor<FindNamedClassVisitor> {
+public:
+  explicit FindNamedClassVisitor(ASTContext *Context)
+    : Context(Context) {}
+
+  bool VisitNamedDecl( NamedDecl *Declaration) {
+    llvm::outs() << "Decl Name: " << Declaration->getQualifiedNameAsString() << "\n";
+    return true;
+  }
+
+  bool VisitAttr( Attr *Attribute) {
+    llvm::outs() << "Attr Name: " << Attribute->getSpelling() << "\n";
+    return true;
+  }
+
+  bool VisitType( Type *T) {
+    llvm::outs() << "Type Name: " << T->getTypeClassName() << "\n";
+    return true;
+  }
+
+private:
+  ASTContext *Context;
+};
+
+class FindNamedClassConsumer : public clang::ASTConsumer {
+public:
+  explicit FindNamedClassConsumer(ASTContext *Context)
+    : Visitor(Context) {}
+
+  virtual void HandleTranslationUnit(clang::ASTContext &Context) {
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+private:
+  FindNamedClassVisitor Visitor;
+};
+
+class FindNamedClassAction : public clang::ASTFrontendAction {
+public:
+  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+    clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
+    return std::unique_ptr<clang::ASTConsumer>(
+        new FindNamedClassConsumer(&Compiler.getASTContext()));
+  }
+};
+
+int main(int argc, char **argv) {
+  if (argc > 1) {
+    clang::tooling::runToolOnCode(new FindNamedClassAction, argv[1]);
+  }
+}
