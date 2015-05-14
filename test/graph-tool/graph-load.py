3559b7e721559b6dd524758a83cf90c1c1d25044
Kevin Rowland <krowlan3@jhu.edu>
2015-05-14 19:16:45 -0400

    subgraph isomorphism working, finding and highlighting a function call in a new png called iso-embed.png. I modified the broken matrixmult.dot file so that a function call actually existed in the AST

--- test/graph-tool/graph-load.py
+++ test/graph-tool/graph-load.py
@@ -16,6 +16,7 @@ from graph_tool.all import *
 # Create a new graph object
 g = Graph()
 
+# TODO use internal property maps?
 # Creating empty maps for vertex and graph properties
 vprop_node = g.new_vertex_property("string")	# Dot file node name
 vprop_label = g.new_vertex_property("string")	# Clang AST labels
@@ -25,7 +26,7 @@ vprop_value = g.new_vertex_property("string")	# Value if operator
 
 gprop_name = g.new_graph_property("string")		# Dot file - digraph name
 
-
+# Some helper functions ----------------------------------------------------
 def get_lvalue(tkn):
 	idx = tkn.find("=")
 	if(idx == -1):
@@ -50,7 +51,7 @@ def store_properties(v, prop, val):
 		vprop_value[v] = val
 	elif(prop == "type"):
 		vprop_type[v] = val
-
+# End helper functions -----------------------------------------------------
 
 # To make the vertex reading more robust, consider doing the following:
 #   1) Strip all white-space
@@ -66,12 +67,13 @@ def store_properties(v, prop, val):
 #   4)shape=record		-	label="{CompoundStmt}"
 #   5)shape : record	-	label : CompoundStmt
 
+# TODO - get file from arguments
 # Open the dot file, iterate over lines, save vertices
 with open("../opencl/matrixmult.dot") as f:
 	for line in f:
 		tkns = line.split()
 		if(len(tkns) < 2):
-			print "blank line" # TODO do nothing in if: block
+			pass
 		# Name the graph
 		elif(tkns[0] == 'digraph'):
 			gprop_name[g] = tkns[1]
@@ -79,7 +81,6 @@ with open("../opencl/matrixmult.dot") as f:
 		elif(tkns[1] != "->"):
 			v = g.add_vertex()
 			vprop_node[v] = tkns[0]
-			print vprop_node[v]
 			for tkn in tkns[1:]:
 				prop = get_lvalue(tkn)
 				val  = get_rvalue(tkn)
@@ -91,29 +92,52 @@ with open("../opencl/matrixmult.dot") as f:
 	for line in f:
 		tkns = line.split()
 		if(len(tkns) == 3 and tkns[1] == "->"):
-			print tkns[0] + "     " + tkns[2]
 			v1 = find_vertex(g, vprop_node, tkns[0])[0]
-			print v1
 			v2 = find_vertex(g, vprop_node, tkns[2].strip(';'))[0]
-			print v2
 			g.add_edge(v1, v2)
 
-# Print the graph's vertices
-
-
-# Draw the graph with some vertex appearance options
+# Draw the original graph with some vertex appearance options
 graph_draw(g, vertex_size=15, vertex_text=vprop_node, vertex_font_size=14,
 		output_size=(1000, 1000), output="dot-file.png")
 
+#-----------------------------------------------------------------------------
 
+# Replace function calls with variables using subgraph_isomorphism()
+sub = Graph()
+subprop_label = sub.new_vertex_property("string")
 
+# The function call expression
+v0 = sub.add_vertex()
+subprop_label[v0] = "CallExpr"
+# And its children...
+# ... the name of the function
+v1 = sub.add_vertex()
+subprop_label[v1] = "DeclRefExpr"
+sub.add_edge(v0, v1)
+# ... and the argument (?)
+v2 = sub.add_vertex()
+subprop_label[v2] = "IntegerLiteral"
+sub.add_edge(v0,v2)
 
+# Get property map of isomorphism
+iso = subgraph_isomorphism(sub, g, vertex_label=[subprop_label, vprop_label])
 
+# Set different vertex properties for the isomorphism only
+# TODO - replace this with code that turns function calls into variables
+for i in range(len(iso)):
+	g.set_vertex_filter(None)
+	g.set_edge_filter(None)
+	vmask, emask = mark_subgraph(g, sub, iso[i])
+	g.set_vertex_filter(vmask)
+	g.set_edge_filter(emask)
+g.set_vertex_filter(None)
+g.set_edge_filter(None)
 
+# Draw the subgraph with some vertex appearance options
+graph_draw(sub, vertex_size=15, vertex_text=subprop_label, vertex_font_size=14,
+		output_size=(1000, 1000), output="subgraph-isomorphism.png")
 
-
-
-
-
-
-
+# Draw the graph with the subgraph embedded
+graph_draw(g, vertex_size=15, vertex_text=vprop_node, vertex_font_size=14,
+		vertex_fill_color=vmask, edge_color=emask,
+		output_size=(1000, 1000), output="iso-embed.png")
